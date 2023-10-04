e25b9b146bb104f49b28d3bf1aabac632dcc5e26
Aldi setiawan <114683945+Aldistwn29@users.noreply.github.com>
2023-10-04 12:35:38 +0700

    Add files via upload

---
+++ simulasi.js
@@ -0,0 +1,12 @@
+const profile ={
+    Nama: "Aldi Setiawan",
+    Nim: 20220040054,
+    Prodi: "Teknik Inromatika",
+    Alamat: "Kp Pasir Bentik",
+    TTL: "Sukabumi-29-Maret-2003"
+}
+console.log("Nama Saya: " +profile.Nama);
+console.log("Nim: " + profile.Nim);
+console.log("Prodi: " + profile.Prodi);
+console.log("Alamat: " + profile.Alamatlamat);
+console.log("Tempat-Tanggal Lahir: " + profile.TTL);
